9e7d43bc9187cb8a34132faa5e4e7e023df3cf73
Nikhil <nsivapur@uwo.ca>
2024-02-29 00:21:24 -0500

    completed

--- src/components/BirthDateComponent.js
+++ src/components/BirthDateComponent.js
@@ -1,9 +1,11 @@
-import React, {useState} from "react";
+import React, {useEffect, useState, useRef} from "react";
 
-const BirthDateComponent = () => {
+const BirthDateComponent = ({ setDate, error, reset }) => {
     const [day, setDay] = useState('');
     const [month, setMonth] = useState('');
     const [year, setYear] = useState('');
+    const [hasUserSelected, setHasUserSelected] = useState(false);
+    const isInitialMount = useRef(true);
   
     const days = Array.from({ length: 31 }, (_, i) => i + 1);
     const months = [
@@ -12,44 +14,97 @@ const BirthDateComponent = () => {
     ];
     const years = Array.from({ length: 100 }, (_, i) => new Date().getFullYear() - i);
 
+    const dispatchDateChangeEvent = () => {
+        if(hasUserSelected) {
+            const formattedDay = day || '';
+            const formattedMonth = month || '';
+            const formattedYear = year || '';
+    
+            // synthetic event object
+            const syntheticEvent = {
+                target: {
+                    name: "date_of_birth",
+                    value: `${formattedDay}-${formattedMonth}-${formattedYear}`
+                }
+            };
+            // console.log(syntheticEvent);
+            setDate(syntheticEvent);
+        }
+    };
+
+    useEffect(() => {
+        if (isInitialMount.current) {
+            isInitialMount.current = false;
+        } else {
+            dispatchDateChangeEvent();
+        }
+    }, [day, month, year]);
+
+    useEffect(() => {
+        if (reset) {
+            setDay('');
+            setMonth('');
+            setYear('');
+            setHasUserSelected(false);
+        }
+    }, [reset]);
+
+
+    const handleDayChange = (e) => {
+        setDay(e.target.value);
+        setHasUserSelected(true);
+    };
+    const handleMonthChange = (e) => {
+        setMonth(e.target.value);
+        setHasUserSelected(true);
+    };
+    const handleYearChange = (e) => {
+        setYear(e.target.value);
+        setHasUserSelected(true);
+    };
+
     return (
-        <div className="mb-3">
-            <label className="block text-gray-700 font-bold mb-2" htmlFor="birthdate">
+        <div className="flex flex-col mb-3">
+            <label className="block text-gray-700 font-bold mb-2 text-sm" htmlFor="birthdate">
             Birthdate
-            <span className="text-red-500 font-lato"> *</span>
+            <span className="text-red-500"> *</span>
             </label>
-            <div className="flex space-x-2">
-            <select 
-                className="block w-full px-4 py-2 border border-[#A5B6CD] rounded-md shadow-sm focus:outline-none focus:ring-[#127C95] focus:border-[#127C95] sm:text-sm"
-                value={day}
-                onChange={(e) => setDay(e.target.value)}
-            >
-                <option value="">Day</option>
-                {days.map((d) => (
-                <option key={d} value={d}>{d}</option>
-                ))}
-            </select>
-            <select 
-                className="block w-full px-4 py-2 border border-[#A5B6CD] rounded-md shadow-sm focus:outline-none focus:ring-[#127C95] focus:border-[#127C95] sm:text-sm"
-                value={month}
-                onChange={(e) => setMonth(e.target.value)}
-            >
-                <option value="">Month</option>
-                {months.map((m, index) => (
-                <option key={m} value={index + 1}>{m}</option>
-                ))}
-            </select>
-            <select 
-                className="block w-full px-4 py-2 border border-[#A5B6CD] rounded-md shadow-sm focus:outline-none focus:ring-[#127C95] focus:border-[#127C95] sm:text-sm"
-                value={year}
-                onChange={(e) => setYear(e.target.value)}
-            >
-                <option value="">Year</option>
-                {years.map((y) => (
-                <option key={y} value={y}>{y}</option>
-                ))}
-            </select>
+            <div className="flex justify-between space-x-1 md:space-x-2">
+                <select 
+                    className="flex-1 appearance-none form-select bg-no-repeat bg-transparent pl-4 py-3 border border-[#A5B6CD] rounded-md 
+                    focus:outline-none focus:ring-[#127C95] focus:border-[#127C95] text-sm max-w-[30%]"
+                    value={day}
+                    onChange={handleDayChange}
+                >
+                    <option value="">Day</option>
+                    {days.map((d) => (
+                    <option key={d} value={d}>{d}</option>
+                    ))}
+                </select>
+                <select 
+                    className="flex-1 appearance-none form-select bg-no-repeat bg-transparent pl-4 border border-[#A5B6CD] rounded-md
+                    focus:outline-none focus:ring-[#127C95] focus:border-[#127C95] text-sm max-w-[30%]"
+                    value={month}
+                    onChange={handleMonthChange}
+                >
+                    <option value="">Month</option>
+                    {months.map((m, index) => (
+                    <option key={m} value={index + 1}>{m}</option>
+                    ))}
+                </select>
+                <select 
+                    className="flex-1 appearance-none form-select bg-no-repeat bg-transparent pl-4 border border-[#A5B6CD] rounded-md 
+                    focus:outline-none focus:ring-[#127C95] focus:border-[#127C95] text-sm max-w-[30%]"
+                    value={year}
+                    onChange={handleYearChange}
+                >
+                    <option value="">Year</option>
+                    {years.map((y) => (
+                    <option key={y} value={y}>{y}</option>
+                    ))}
+                </select>
             </div>
+            <p className="text-red-500 font-lato text-xs">{error}</p>
         </div>
     )
 }
--- src/components/InputComponent.js
+++ src/components/InputComponent.js
@@ -1,30 +1,49 @@
 import React, {useState} from "react";
 
 const InputComponent = ({ label, name, type, value, placeholder, onChange, onBlur, error, pattern }) => {
+  const [isFocused, setIsFocused] = useState(false);
 
-    return (
-      <div className="my-3">
-        <label className="block text-gray-700 font-semibold mb-2 font-lato" htmlFor={label}>
-          {label}
-          <span className="text-red-500 font-lato"> *</span>
-        </label>
-        <input
-          type={type}
-          name={name}
-          value={value}
-          onChange={onChange}
-          onBlur={onBlur}
-          pattern={pattern}
-          className={`appearance-none border border-[#A5B6CD] rounded w-full py-2 px-3 text-gray-700 focus:outline-none 
-            ${
-            error ? 'border-red-500' : ''
-            }
-            `}
-          placeholder={placeholder}
-        />
-        {error && <p className="text-red-500 text-xs">{error}</p>}
-      </div>
-    );
+  const handleFocus = (e) => {
+      setIsFocused(true);
+      // if (onFocus) { 
+      //   onFocus(e);
+      // }
   };
-  
-  export default InputComponent;
+
+  const handleBlur = (e) => {
+      setIsFocused(false);
+      if (onBlur) onBlur(e);
+  };
+
+  const labelClass = isFocused || value
+      ? `absolute text-sm ${error ? `text-red-500` : `text-[#A5B6CD]`} transform -translate-y-2.5 scale-75 origin-[0] bg-white px-1`
+      : "absolute transform scale-0 -translate-y-2.5 px-1";
+
+  return (
+    <div className="my-3 relative">
+      <label className="block text-gray-700 font-semibold mb-2 font-lato text-sm" htmlFor={label}>
+          {label}
+          <span className="text-red-500"> *</span>
+      </label>
+      <label className={`${labelClass} left-2 transition-all`} htmlFor={name}>
+        {label}
+        <span className="text-red-500"> *</span>
+      </label>
+      <input
+        type={type}
+        name={name}
+        value={value}
+        onChange={onChange}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        pattern={pattern}
+        className={`appearance-none border border-[#A5B6CD] rounded-md w-full py-2 px-3 focus:outline-none 
+          ${error ? 'border-red-500' : ''}`}
+        placeholder={placeholder}
+      />
+      {error && <p className="text-red-500 text-xs">{error}</p>}
+    </div>
+  );
+};
+
+export default InputComponent;
